b9775f4e74e283dcd577a3bf1c272f000e5ef4e4
Raushan kumar <86125144+raushan9jnv@users.noreply.github.com>
2022-08-29 22:02:30 +0530

    create a todo class to represent todos

--- FrontEnd/todo/src/app/list-todos/list-todos.component.ts
+++ FrontEnd/todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo{
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ){
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,10 +19,14 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos =[
-    {id : 1, description : 'Learn to Dance'},
-    {id : 2, description : 'become an Expert at Angular'},
-    {id : 3, description : 'Visit India'},
-    {id : 4, description : 'Raushan is Here'}
+    new Todo(1,'Learn to Dance',false, new Date()),
+    new Todo(2,'become an Expert at Angular',false, new Date()),
+    new Todo(3,'Visit India',false, new Date()),
+    new Todo(4,'Raushan is Here',false, new Date())
+    // {id : 1, description : 'Learn to Dance'},
+    // {id : 2, description : 'become an Expert at Angular'},
+    // {id : 3, description : 'Visit India'},
+    // {id : 4, description : 'Raushan is Here'}
   ]
 
   // todo={

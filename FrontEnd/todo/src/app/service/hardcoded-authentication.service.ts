80d783fbb7c6a678cfd7c4534b202907cfcc917b
Raushan kumar <86125144+raushan9jnv@users.noreply.github.com>
2022-08-30 18:34:44 +0530

    used sessionStorage for login

--- FrontEnd/todo/src/app/service/hardcoded-authentication.service.ts
+++ FrontEnd/todo/src/app/service/hardcoded-authentication.service.ts
@@ -8,10 +8,18 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username, password){
+    console.log('before ' + this.isUserLoggedIn())
     if(username==="in28minutes" && password==="dummy"){
+      sessionStorage.setItem('authenticateUser', username);
+      console.log('after ' + this.isUserLoggedIn())
       return true;
     }
     return false;
 
+  }
+
+  isUserLoggedIn(){
+    let user = sessionStorage.getItem('authenticateUser')
+    return !(user===null)
   }
 }

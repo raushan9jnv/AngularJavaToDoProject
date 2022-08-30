0be7875d67ff3d3434a4da7b920dc76f70ef4b14
Raushan kumar <86125144+raushan9jnv@users.noreply.github.com>
2022-08-30 18:08:24 +0530

    created an independent authentication service

--- FrontEnd/todo/src/app/login/login.component.ts
+++ FrontEnd/todo/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,7 @@ export class LoginComponent implements OnInit {
   //Router
   //Angular.giveMeRouter
   //Dependency Injection
-  constructor(private router :Router) { }
+  constructor(private router :Router, private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
   ngOnInit() {
   }
@@ -25,7 +26,9 @@ export class LoginComponent implements OnInit {
     // console.log(this.username);
     // console.log(this.password);   we don't print password..mot goood pratice
 
-    if(this.username==="in28minutes" && this.password==="dummy"){
+    // if(this.username==="in28minutes" && this.password==="dummy"){
+    if(this.hardcodedAuthenticationService.authenticate(this.username, this.password)){
+      //redirect to welcome page
       this.router.navigate(['welcome',this.username])
       this.invalidLogin = false
     }else{

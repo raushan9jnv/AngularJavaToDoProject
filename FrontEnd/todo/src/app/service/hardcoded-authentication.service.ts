bc3a677001e1492dae908b27ae6fac9cf1a17c97
Raushan kumar <86125144+raushan9jnv@users.noreply.github.com>
2022-08-30 18:55:48 +0530

    enabling menu links based on user authentication

--- FrontEnd/todo/src/app/service/hardcoded-authentication.service.ts
+++ FrontEnd/todo/src/app/service/hardcoded-authentication.service.ts
@@ -8,10 +8,10 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username, password){
-    console.log('before ' + this.isUserLoggedIn())
+    // console.log('before ' + this.isUserLoggedIn())
     if(username==="in28minutes" && password==="dummy"){
       sessionStorage.setItem('authenticateUser', username);
-      console.log('after ' + this.isUserLoggedIn())
+      // console.log('after ' + this.isUserLoggedIn())
       return true;
     }
     return false;

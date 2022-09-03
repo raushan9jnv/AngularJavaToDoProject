b40e4a872942cade69e4d899d38c5b8f539d7db1
Raushan kumar <86125144+raushan9jnv@users.noreply.github.com>
2022-09-03 16:47:22 +0530

    PathVariable

--- BackEnd/todo-backend/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ BackEnd/todo-backend/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +24,12 @@ public class HelloWorldController {
 		public HelloWorldBean helloWorldBean() {
 			return new HelloWorldBean("Hello World");  //this bean automatically converted to JSON
 		}
+		
+		//hello-world/path-variable/in28minutes
+		@GetMapping(path = "/hello-world/path-variable/{name}")
+		public HelloWorldBean helloWorldPathVaibale(@PathVariable String name) {
+			return new HelloWorldBean(String.format("Hello World, %s", name)); 
+			//http://localhost:8080/hello-world/path-variable/in28minutes   -->     {"message":"Hello World, in28minutes"}
+		}
 }
+

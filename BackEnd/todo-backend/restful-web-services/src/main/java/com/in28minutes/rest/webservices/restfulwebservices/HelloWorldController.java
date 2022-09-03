58763450ba581785c33ff880aa0c3c5148b98fae
Raushan kumar <86125144+raushan9jnv@users.noreply.github.com>
2022-09-03 15:39:30 +0530

    changed to GetMapping

--- BackEnd/todo-backend/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ BackEnd/todo-backend/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ public class HelloWorldController {
 		//URI- /hello-world
 		// method - "Hello World"
 	
-		@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+		@GetMapping(path = "/hello-world")
 		public String helloWorld() {
 			return "Hello World";
 		}

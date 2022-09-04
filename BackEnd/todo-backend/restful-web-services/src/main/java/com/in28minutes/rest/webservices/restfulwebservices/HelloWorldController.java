8f515ebe24bf393d528fc73ac0f8055290911bfa
Raushan kumar <86125144+raushan9jnv@users.noreply.github.com>
2022-09-04 10:56:52 +0530

    @CrossOrigin(Origin='http://localhostAddress') give permisson to frontend to use backend api

--- BackEnd/todo-backend/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ BackEnd/todo-backend/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
+@CrossOrigin(origins="http://localhost:4200") //give permission to frontend to use api --> origins define handle only for this URL only
 @RestController
 public class HelloWorldController {
 
